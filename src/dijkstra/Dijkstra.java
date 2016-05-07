ebf4aedb01f7783d6ae6c7b09bf7895e53c73611
dario.lwoloszin <dario.lwoloszin@CAS419853W0705.senacsp.edu.br>
2016-05-06 22:36:12 -0300

    inicio Dijkstra

---
+++ src/dijkstra/Dijkstra.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dijkstra;
+
+/**
+ *
+ * @author dario.lwoloszin
+ */
+public class Dijkstra {
+    private Grafo grafo;
+    private Vertice inicio;
+    private Lista listademenorcusto;
+    
+    Dijkstra(Grafo grafo, Vertice inicio){
+        this.grafo = grafo;
+        this.inicio = inicio;
+        
+    
+    
+    }
+    
+    
+}
